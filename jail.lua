bf5bb6b95e04d41c53e9668d9feb5e900a4988ea
MrMemoL <61466183+MrMemoL@users.noreply.github.com>
2020-12-25 21:54:15 +0300

    Add files via upload

---
+++ jail.lua
@@ -0,0 +1,140 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+local TextLabel_7 = Instance.new("TextLabel")
+local TextLabel_8 = Instance.new("TextLabel")
+local TextLabel_9 = Instance.new("TextLabel")
+local TextLabel_10 = Instance.new("TextLabel")
+local TextLabel_11 = Instance.new("TextLabel")
+local TextLabel_12 = Instance.new("TextLabel")
+local TextLabel_13 = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+TextLabel.Parent = ScreenGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0.0118389893, 0, 0.300982803, 0)
+TextLabel.Size = UDim2.new(0, 353, 0, 192)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+TextLabel_2.Parent = ScreenGui
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.Position = UDim2.new(0.0118389893, 0, 0.536855042, 0)
+TextLabel_2.Size = UDim2.new(0, 353, 0, 192)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 14.000
+
+TextLabel_3.Parent = ScreenGui
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.Position = UDim2.new(0.290449888, 0, 0.0651105568, 0)
+TextLabel_3.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 14.000
+
+TextLabel_4.Parent = ScreenGui
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.Position = UDim2.new(0.290449888, 0, 0.300982803, 0)
+TextLabel_4.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_4.Font = Enum.Font.SourceSans
+TextLabel_4.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextSize = 14.000
+
+TextLabel_5.Parent = ScreenGui
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.Position = UDim2.new(0.290449888, 0, 0.536855042, 0)
+TextLabel_5.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_5.Font = Enum.Font.SourceSans
+TextLabel_5.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextSize = 14.000
+
+TextLabel_6.Parent = ScreenGui
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.Position = UDim2.new(0.614048958, 0, 0.536855042, 0)
+TextLabel_6.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_6.Font = Enum.Font.SourceSans
+TextLabel_6.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextSize = 14.000
+
+TextLabel_7.Parent = ScreenGui
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.Position = UDim2.new(0.614048958, 0, 0.300982803, 0)
+TextLabel_7.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_7.Font = Enum.Font.SourceSans
+TextLabel_7.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.TextSize = 14.000
+
+TextLabel_8.Parent = ScreenGui
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.Position = UDim2.new(0.614048958, 0, 0.0651105568, 0)
+TextLabel_8.Size = UDim2.new(0, 410, 0, 192)
+TextLabel_8.Font = Enum.Font.SourceSans
+TextLabel_8.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.TextSize = 14.000
+
+TextLabel_9.Parent = ScreenGui
+TextLabel_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_9.Position = UDim2.new(0.0118389893, 0, 0.300982803, 0)
+TextLabel_9.Size = UDim2.new(0, 353, 0, 192)
+TextLabel_9.Font = Enum.Font.SourceSans
+TextLabel_9.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_9.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_9.TextSize = 14.000
+
+TextLabel_10.Parent = ScreenGui
+TextLabel_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_10.Position = UDim2.new(0.0118389893, 0, 0.0651105642, 0)
+TextLabel_10.Size = UDim2.new(0, 353, 0, 192)
+TextLabel_10.Font = Enum.Font.SourceSans
+TextLabel_10.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_10.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_10.TextSize = 14.000
+
+TextLabel_11.Parent = ScreenGui
+TextLabel_11.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_11.Position = UDim2.new(0.0118389893, 0, 0.772727251, 0)
+TextLabel_11.Size = UDim2.new(0, 353, 0, 177)
+TextLabel_11.Font = Enum.Font.SourceSans
+TextLabel_11.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_11.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_11.TextSize = 14.000
+
+TextLabel_12.Parent = ScreenGui
+TextLabel_12.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_12.Position = UDim2.new(0.290449888, 0, 0.772727251, 0)
+TextLabel_12.Size = UDim2.new(0, 410, 0, 177)
+TextLabel_12.Font = Enum.Font.SourceSans
+TextLabel_12.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_12.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_12.TextSize = 14.000
+
+TextLabel_13.Parent = ScreenGui
+TextLabel_13.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_13.Position = UDim2.new(0.614048958, 0, 0.772727251, 0)
+TextLabel_13.Size = UDim2.new(0, 410, 0, 177)
+TextLabel_13.Font = Enum.Font.SourceSans
+TextLabel_13.Text = "Mal Bora Hack Yapma Sikik Bebe!"
+TextLabel_13.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_13.TextSize = 14.000
